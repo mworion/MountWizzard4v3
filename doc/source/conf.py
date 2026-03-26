77b264f5ca94d550070df7be4ef0a0291408bc5f
mworion <michael.wuertenberger@t-online.de>
2026-03-26 14:28:35 +0100

    add docs for stellarmate

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2023, mworion'
 author = 'Michael Würtenberger'
 
 # The full version, including alpha/beta/rc tags
-version = '3.2.8.3b4'
-release = '3.2.8.3b4'
+version = '3.2.8.3a0'
+release = '3.2.8.3a0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
